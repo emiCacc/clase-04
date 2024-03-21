29ee323dea46663ed51f2d982951a886c563b3a3
emiliano.morales <emiliano.morales@bizitglobal.com>
2024-03-21 15:04:50 -0300

    cambios temporales en header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  tittle = "Colegio Hogwarts de magia y hechicería"
+  tittle = "COLEGIO HOGWARTS DE MAGIA Y HECHICERÍA";
+  califications = "CALIFICACIONES"
 }
