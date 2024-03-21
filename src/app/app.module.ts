5ade980dba7b7b25ea2141155df89604c0c5aa15
emiliano.morales <emiliano.morales@bizitglobal.com>
2024-03-21 02:50:53 -0300

    logica para botones y propiedades responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importa BrowserAnimationsModule
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,19 +12,21 @@ import { AlumnosComponent } from './components/alumnos/alumnos.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LobbyComponent } from './components/lobby/lobby.component';
 
-import { ButtonModule } from 'primeng/button';
-
 @NgModule({
   declarations: [
     AppComponent,
     AlumnosComponent,
     HeaderComponent,
-    LobbyComponent
+    LobbyComponent    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    DropdownModule,
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/lobby/lobby.component.ts
+++ src/app/components/lobby/lobby.component.ts
@@ -8,36 +8,85 @@ import { Component } from '@angular/core';
 
 export class LobbyComponent {
 
-  requestPociones(){    
+  pociones: boolean = false;
+  defensa: boolean = false;
+  encantamientos: boolean = false;
+  historia: boolean = false;
+  quidditch: boolean = false;
+  criaturas: boolean = false;
+  aritmancia: boolean = false;
+  adivinacion: boolean = false;
 
+  materiaSeleccionada: string = '';
+
+  dropdownOptions = [
+    { label: 'Materias obligatorias', value: 'obligatorias' },
+    { label: 'Materias opcionales', value: 'opcionales' }
+  ];
+
+  constructor() { }
+
+  onDropdownChange(event: any) {
+    this.materiaSeleccionada = event.value;
+    const opcionSeleccionada = this.dropdownOptions.find(opcion => opcion.value === event.value);
+  }
+
+  resetButtons() {
+    this.pociones = false;
+    this.defensa = false;
+    this.encantamientos = false;
+    this.historia = false;
+    this.quidditch = false;
+    this.criaturas = false;
+    this.aritmancia = false;
+    this.adivinacion = false;
+  }
+
+  requestPociones() {   
+    console.log("click pociones")
+    this.resetButtons(); 
+    this.pociones = true;
   }
 
   requestDefensa(){    
-
+    console.log("click defensa")
+    this.resetButtons();
+    this.defensa = true;
   }
 
-  requestEncantamientos(){    
-
+  requestEncantamientos(){ 
+    console.log("click encantamientos")
+    this.resetButtons();   
+    this.encantamientos = true;
   }
 
-  requestHistoria(){    
-
+  requestHistoria(){   
+    console.log("click historia")
+    this.resetButtons(); 
+    this.historia = true;
   }
 
   requestQuidditch(){
-
+    console.log("click quidditch")
+    this.resetButtons();
+    this.quidditch = true;
   }
   
   requestCriaturas(){   
-
+    console.log("click criaturas")
+    this.resetButtons();
+    this.criaturas = true;
   }
   
   requestAritmancia(){   
-
+    console.log("click aritmancia")
+    this.resetButtons();
+    this.aritmancia = true;
   }
   
-  requestAdivinacion(){    
-    
+  requestAdivinacion(){  
+    console.log("click adivinacion")
+    this.resetButtons();  
+    this.adivinacion = true;
   }
-
 }
